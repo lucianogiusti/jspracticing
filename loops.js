7b3dc4fcff17f4ac2edd0783bc26d39dbeccb0d3
Luciano Giusti <lucianogiusti@outlook.com.ar>
2020-11-27 11:51:15 -0300

    loops in loops

--- loops.js
+++ loops.js
@@ -1,13 +1,3 @@
-const jonas = [
-    'Tom',
-    'Riddle',
-    true,
-    46,
-    'teacher',
-    false,
-]
-
-
 // loop example
 
 const years = [1991, 2000, 1969, 1995];
@@ -20,6 +10,7 @@ for (let i=0; i < years.length; i++) {
 console.log(ages);
 
 // continue and break examples
+
 console.log('only strings');
 for (let i=0; i < years.length; i++) {
     if (typeof jonas[i] !== 'string') continue;
@@ -31,4 +22,19 @@ console.log('break with number');
 for (let i=0; i < years.length; i++) {
     if (typeof jonas[i] === 'number') break;
     console.log(jonas[i], typeof jonas[i]);
+}
+
+// looping backwards
+
+for (lei i = jonas.length - 1; i>=0; i--) {
+    console.log(i, jonas[i]);
+};
+
+// loops in loops
+
+for (let exe = 1; exe < 4; exe++) {
+    console.log(`--starting exe ${exe}`);
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exe ${exe}: lifting weight rep ${rep}`);
+    }
 }
